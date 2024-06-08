06e98d733e4a6bd015c41a6f1ee6e7270159dbed
nguyenhoanghiep478 <nguyenhoanghiep478@gmail.com>
2024-06-08 18:50:54 +0700

    Deploy to GitHub Pages

---
+++ DACS/js/audioHelper.js
@@ -0,0 +1,152 @@
+﻿window.audioHelper = {
+    play: function (audioElement) {
+        audioElement.play();
+    },
+    pause: function (audioElement) {
+        audioElement.pause();
+    }, 
+    playAudio: function () {
+        var audioPlayer = document.getElementById('audioPlayer');
+        if (audioPlayer) {
+            audioPlayer.play();
+        } else {
+            console.error('Audio player element not found');
+        }
+    },
+    pauseAudio: function () {
+        var audioPlayer = document.getElementById('audioPlayer');
+        if (audioPlayer) {
+            audioPlayer.pause();
+        } else {
+            console.error('Audio player element not found');
+        }
+    },
+    getCurrentTime: function (audioElement) {
+        return audioElement.currentTime || 0;
+    },
+    setCurrentTime: function (audioElement, time) {
+        audioElement.currentTime = time;
+    },
+    getDuration: function (audioElement) {
+        return new Promise((resolve) => {
+            if (audioElement.duration > 0) {
+                resolve(audioElement.duration);
+            } else {
+                audioElement.addEventListener('loadedmetadata', () => {
+                    resolve(audioElement.duration);
+                });
+            }
+        });
+    },
+    getAudioDuration: function (url) {
+        return new Promise((resolve, reject) => {
+            let audio = new Audio(url);
+            audio.onloadedmetadata = function () {
+                resolve(audio.duration);
+            };
+            audio.onerror = function () {
+                reject("Error loading audio file.");
+            };
+        });
+    },
+
+    addTimeUpdateEvent: function (audioElement, dotNetHelper) {
+        audioElement.addEventListener('timeupdate', () => {
+            dotNetHelper.invokeMethodAsync('UpdateCurrentTime', audioElement.currentTime);
+        });
+    },
+    initializeProgressBar: function (progressContainer, audioElement, dotNetHelper) {
+        let isDragging = false;
+
+        progressContainer.addEventListener('mousedown', (e) => {
+            isDragging = true;
+            updateProgress(e);
+        });
+
+        window.addEventListener('mousemove', (e) => {
+            if (isDragging) {
+                updateProgress(e);
+            }
+        });
+
+        window.addEventListener('mouseup', (e) => {
+            if (isDragging) {
+                isDragging = false;
+                updateProgress(e);
+            }
+        });
+
+        progressContainer.addEventListener('click', (e) => {
+            updateProgress(e);
+        });
+
+        function updateProgress(e) {
+            const rect = progressContainer.getBoundingClientRect();
+            const offsetX = e.clientX - rect.left;
+            const progress = Math.max(0, Math.min(1, offsetX / rect.width));
+            const percentage = progress * 100;
+            dotNetHelper.invokeMethodAsync('UpdateProgress', percentage);
+            const newTime = progress * audioElement.duration;
+            audioElement.currentTime = newTime;
+        }
+    },
+    setAudioSource: function (audioElement, url) {
+        audioElement.src = url;
+        audioElement.load();
+        audioElement.oncanplaythrough = function () {
+            audioElement.play().catch(error => {
+                console.error("Error playing audio:", error);
+            });
+        };
+    },
+    
+   
+};
+window.volumeHelper = {
+    initializeVolumeSlider: function (sliderContainer, dotNetHelper) {
+        let isDragging = false;
+
+        sliderContainer.addEventListener('mousedown', (e) => {
+            isDragging = true;
+            updateVolume(e);
+        });
+
+        window.addEventListener('mousemove', (e) => {
+            if (isDragging) {
+                updateVolume(e);
+            }
+        });
+
+        window.addEventListener('mouseup', () => {
+            if (isDragging) {
+                isDragging = false;
+                
+            }
+        });
+        sliderContainer.addEventListener('click', (e) => {
+            updateVolume(e);
+        });
+        function updateVolume(e) {
+            const rect = sliderContainer.getBoundingClientRect();
+            const offsetX = e.clientX - rect.left;
+            const volume = Math.max(0, Math.min(1, offsetX / rect.width));
+            const percentage = volume * 100;
+            dotNetHelper.invokeMethodAsync('UpdateVolume', percentage);
+            const newVolume = percentage;
+            sliderContainer.volumePercentage = newVolume;
+
+        }
+    },
+    setVolume: function (volume) {
+        const audioElements = document.querySelectorAll('audio');
+        audioElements.forEach(audio => {
+            audio.volume = volume;
+        });
+    },
+};
+
+
+
+
+
+
---
+++ DACS/js/showError.js
@@ -0,0 +1,64 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    let toastBox = document.getElementById('toastBox');
+    let successMsg = '<i class="fa-solid fa-circle-check"></i>';
+    let errorMsg = '<i class="fa-solid fa-circle-exclamation"></i>'
+    window.showToast = function (msg) {
+        let toast = document.createElement('div');
+        toast.classList.add('toast');
+        toast.classList.add('show');
+        toast.innerHTML = successMsg + msg;
+        if (toastBox) {
+            toastBox.appendChild(toast);
+        } else {
+            console.error("Không tìm thấy phần tử toastBox!");
+        }
+
+        setTimeout(() => {
+            toast.remove();
+        }, 3000);
+    };
+    window.showToastErr = function (msg) {
+        let toast = document.createElement('div');
+        toast.classList.add('toast');
+        toast.classList.add('show');
+        toast.classList.add('error');
+        toast.innerHTML = errorMsg + msg;
+        if (toastBox) {
+            toastBox.appendChild(toast);
+        } else {
+            console.error("Không tìm thấy phần tử toastBox!");
+        }
+
+        setTimeout(() => {
+            toast.remove();
+        }, 3000);
+    };
+
+    const modalBox = document.querySelector('.js-modalBox');
+    const modalBoxBody = document.querySelector('.js-modalBox-body');
+
+    window.showBox = function (inputContent) {
+        modalBoxBody.innerHTML = inputContent;
+        modalBox.style.display = 'flex';
+    };
+
+    document.addEventListener('click', (event) => {
+        if (event.target.classList.contains('js-modalBox-close') ||
+            event.target.classList.contains('js-accept') ||
+            event.target === modalBox) {
+            closeModal(event.target.closest('.js-modalBox'));
+        }
+        if (event.target.classList.contains('js-accept')) {
+            const redirectUrl = event.target.dataset.redirectUrl;
+            if (redirectUrl) {
+                window.location.href = redirectUrl;
+            } else {
+                console.warn('No redirect URL found on js-accept button.');
+            }
+        }
+    });
+
+    function closeModal(modal) {
+        modal.style.display = 'none';
+    }
+});
